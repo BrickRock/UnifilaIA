3a26aa7091a4b1a1a14d1f4120e185fc94d6180c
Marco Olvera <marcolvera333@gmail.com>
2026-04-17 14:42:38 -0600

    endpoint dummy para obtener el estado de la unifila

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
@@ -11,3 +12,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/estado_unifila")
+def get_status():
+    """
+        Permite obtener el estado de la unifila 1 = CLOSED, 2= OPEN, 3= HALF-OPEN 
+    """
+    return random.randint(1,2,3)
